b4a95cbe1f987851eefc6620b1c052178bbb1e20
RuiGeng <ruichen.gengli@georgebrown.ca>
2021-12-12 10:21:42 -0500

    Initial Enemy And Loot Prefabs and Animations

---
+++ Assets/Scripts/CollectableScripts.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableScripts : MonoBehaviour
+{
+    public LootType Type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
+public enum LootType
+{
+    SMALL_GEM,
+    LARGE_GEM,
+    AMMO_CHEST,
+    HEALTH_POTION
+}
